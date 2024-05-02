b50460a621a61280711cfe1b48f2a242d4109f9d
Andrea Pollastri <18210420+andreapollastri@users.noreply.github.com>
2024-05-02 23:55:42 +0200

    Track Fix

--- src/Laracheck.php
+++ src/Laracheck.php
@@ -18,7 +18,13 @@ class Laracheck
     private function sendTrack($exception): void
     {
         try {
-            Http::post(config('laracheck.endpoint'), [
+            $code = ($exception->getCode() == 0) ? 500 : $exception->getCode();
+            Http::withHeaders([
+                'Authorization' => 'Bearer ' . config('laracheck.api_key'),
+                'Accept' => 'application/json',
+                'Content-Type' => 'application/json',
+            ])
+            ->post(config('laracheck.endpoint'), [
                 'site_id' => config('laracheck.site_id'),
                 'env' => config('app.env'),
                 'url' => config('app.url'),
@@ -27,13 +33,10 @@ class Laracheck
                 'user_agent' => request()->header('User-Agent'),
                 'method' => request()->method(),
                 'path' => request()->path(),
-                'code' => $exception->getCode(),
+                'code' => $code,
                 'file' => $exception->getFile(),
                 'line' => $exception->getLine(),
-                'message' => $exception->getMessage(), 
-            ], [
-                'Content-Type' => 'application/json',
-                'Authorization' => 'Bearer '.config('laracheck.api_key'),
+                'message' => $exception->getMessage(),
             ]);
         } catch (\Throwable $th) {
             //
